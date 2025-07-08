c747318da64426f47238e14a9219a3181863f3b7
ENDERVANO <105923137+ENDERVANO@users.noreply.github.com>
2025-07-09 00:13:36 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".swiper", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+Fancybox.bind('[data-fancybox="gallery"]', {
+    Toolbar: {
+        display: {
+            left: ["infobar"],
+            middle: [
+                "zoomIn",
+                "zoomOut",
+                "toggle1to1",
+                "rotateCCW",
+                "rotateCW",
+                "flipX",
+                "flipY",
+            ],
+            right: ["slideshow", "thumbs", "close"],
+        },
+    },
+});
+AOS.init();
+
